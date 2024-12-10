5062fa66bb7ba505e1304a4f921dc87f98929971
purvabhilare <purvabhilare@gmail.com>
2024-12-10 19:06:27 +0000

    added test for Fetch

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -53,3 +53,36 @@ describe("DOM in javascript file", () => {
     expect(addInvFormClass.style.display).toBe("flex");
   });
 }); //Describe for DOM ends here
+
+//fetch Data
+describe("Fetch Data", () => {
+  test("Fetch Inventory data", async () => {
+    const mockProducts = [
+      {
+        name: "Dell XPS 15",
+        brand: "Dell",
+        category: "Laptops",
+        subCategory: "Ultrabooks",
+        price: "2499.99",
+        stock: "25",
+        supplier: "Dell Technologies",
+        supplierEmail: "dell@gmail.com",
+        warranty: "1 Year",
+        lastUpdated: "2024-11-04",
+        id: "PRD001",
+      },
+    ];
+    fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => mockProducts,
+    });
+    const result = await fetchData();
+    expect(result).toEqual(mockProducts);
+    expect(fetch).toHaveBeenCalledWith("http://localhost:3000/products");
+  }); // test for fetch ends here
+
+  test("throw error if fetch api is not successful", async () => {
+    fetch.mockRejectedValueOnce(new Error("Fetch has failed"));
+    await expect(fetchData()).rejects.toThrow("Fetch has failed");
+  });
+}); //Describe for fetch ends here
